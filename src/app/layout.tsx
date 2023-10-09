9c6943627ad136b11e250855668454070dddffb6
Athrael Soju <athrael.soju@gmail.com>
2023-10-09 14:32:25 +0100

    make props readoonly

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ import '../global.css';
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  readonly children: React.ReactNode;
 }) {
   return (
     <html lang="en">
