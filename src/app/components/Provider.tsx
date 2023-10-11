9c051f12e5058fcf9eea326550da83b2fd2be28e
Athrael Soju <athrael.soju@gmail.com>
2023-10-11 16:04:49 +0100

    Mark the props of the component as read-only

--- src/app/components/Provider.tsx
+++ src/app/components/Provider.tsx
@@ -2,6 +2,10 @@
 import { SessionProvider } from 'next-auth/react';
 import React from 'react';
 
-export default function Provider({ children }: { children: React.ReactNode }) {
+export default function Provider({
+  children,
+}: {
+  readonly children: React.ReactNode;
+}) {
   return <SessionProvider>{children}</SessionProvider>;
 }
