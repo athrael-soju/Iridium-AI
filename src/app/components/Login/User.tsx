524edff32124d4572c4f48b92ab7acd5375bdfd5
Athrael Soju <athrael.soju@gmail.com>
2023-10-24 16:29:57 +0100

    minor changes

--- src/app/components/Login/User.tsx
+++ src/app/components/Login/User.tsx
@@ -1,17 +1,13 @@
 import { signIn, signOut, useSession } from 'next-auth/react';
-import {
-  ArrowRightOnRectangleIcon,
-  ArrowLeftOnRectangleIcon,
-  UserCircleIcon,
-} from '@heroicons/react/24/outline';
-import { ActionIcon } from '@/components';
+import { UserCircleIcon } from '@heroicons/react/24/outline';
+import { ActionUserIcon } from '@/components';
 import styled from 'styled-components';
 
 const Image = styled.img`
   display: flex;
   align-items: center;
   text-align: center;
-  max-width: 50px;
+  max-width: 40px;
   border-radius: 50%;
 `;
 
@@ -25,30 +21,21 @@ const User = () => {
     if (!session) {
       return (
         <>
-          <ActionIcon
-            icon={ArrowRightOnRectangleIcon}
-            onClick={() => signIn()}
-            title="Sign in"
-          />
-          <ActionIcon
+          <ActionUserIcon
             icon={UserCircleIcon}
             onClick={() => signIn()}
-            title="Sign in"
+            title="Guest - Sign in"
           />
         </>
       );
     } else if (session?.user) {
       return (
         <>
-          <ActionIcon
-            icon={ArrowLeftOnRectangleIcon}
-            onClick={() => signOut()}
-            title="Sign Out"
-          />
           <Image
             src={session.user.image ?? ''}
             alt="User"
-            title={session.user.email ?? ''}
+            title={session.user.email + ' - Sign Out' || ''}
+            onClick={() => signOut()}
           />
         </>
       );
