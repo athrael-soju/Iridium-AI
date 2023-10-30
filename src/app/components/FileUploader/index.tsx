157f0911befc78b3c76116e3bf9bf5592412acda
vadim.nicolai-EXT <nicolai.vadim@gmail.com>
2023-10-30 10:20:49 +0200

    Small improvement.

--- src/app/components/FileUploader/index.tsx
+++ src/app/components/FileUploader/index.tsx
@@ -1,51 +1,50 @@
 import React from 'react';
 import { CloudArrowUpIcon } from '@heroicons/react/24/outline';
 import { Upload, Tooltip } from 'antd';
-import styled from 'styled-components';
-import useProcessFile from './useProcessFile';
+import useUploadProps from './useUploadProps';
 
 interface FileUploaderProps {
   namespace: any;
 }
 
-const Container = styled.div`
-  .uploadSvg {
-    position: absolute;
-    left: 15px;
-    bottom: 18.5px;
-    cursor: pointer;
-    color: gray;
-    width: 21px;
-    height: auto;
-    z-index: 1;
-  }
-
-  .ant-upload-wrapper {
-    color: #fff !important;
-    display: block !important;
-    margin-bottom: 20px !important;
-  }
-
-  .ant-upload-list-item-name {
-    padding: 0 !important;
-  }
-
-  .ant-upload-icon {
-    display: none !important;
-  }
-`;
-
 export const FileUploader: React.FC<FileUploaderProps> = ({ namespace }) => {
-  const props = useProcessFile({ namespace });
+  const props = useUploadProps({ namespace });
 
   return (
-    <Container>
+    <div className="container">
       <Upload {...props}>
         <Tooltip title="Upload file for ingestion and processing into the database">
           <CloudArrowUpIcon className="uploadSvg" />
         </Tooltip>
       </Upload>
-    </Container>
+      <style jsx>{`
+        // https://github.com/vercel/styled-jsx?tab=readme-ov-file#one-off-global-selectors
+        .container :global(.uploadSvg) {
+          position: absolute;
+          left: 15px;
+          bottom: 18.5px;
+          cursor: pointer;
+          color: gray;
+          width: 21px;
+          height: auto;
+          z-index: 1;
+        }
+
+        .container :global(.ant-upload-wrapper) {
+          color: #fff !important;
+          display: block !important;
+          margin-bottom: 20px !important;
+        }
+
+        .container :global(.ant-upload-list-item-name) {
+          padding: 0 !important;
+        }
+
+        .container :global(.ant-upload-icon) {
+          display: none !important;
+        }
+      `}</style>
+    </div>
   );
 };
 
